eeac3393b5b6316fea777bfd6422bc01c17cccee
sai <saikumar.gyv9880@gmail.com>
2020-01-27 14:03:43 +0530

    added calc function

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(x,y):
+"""Add function"""
+return x+y
+
+
+def subtract(x,y):
+"""Subtract function"""
+return x-y
+
+def mul(x,y):
+"""multiplication function"""
+return x*y
+
+def divi(x,y):
+"""Division function"""
+return x/y
